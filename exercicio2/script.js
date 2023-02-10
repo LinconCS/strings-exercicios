8dbf7872a5badfec9c9356f12952fa5937ae5608
LinconCS <linconsantos@gmail.com>
2023-02-09 21:28:37 -0300

    alteracao

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,22 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = 'Eu não sou supersticioso, mas sou um pouco ________.      ';
+
+function retirarEspacosESubstituir(frase){
+
+const remocao = frase.trim()
+const tamanhoString = frase.length
+const tamanhoStringRemocao = remocao.length
+const substituirNaFrase = remocao.replaceAll('________', 'sticioso') 
+
+console.log(remocao)
+console.log('Quantidade de caracteres na frase sem remoção dos espaços:', tamanhoString)
+console.log('Quantidade de caracteres na frase com remoção dos espaços:', tamanhoStringRemocao)
+console.log(substituirNaFrase)
+}
+
+const novaString = retirarEspacosESubstituir(minhaString)
+
+
+
+
+
+
